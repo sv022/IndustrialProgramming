88fe074e3b30b3f21bd71c24004342752c86934b
sv022 <83920644+sv022@users.noreply.github.com>
2023-02-27 15:12:44 +0300

    Update main.cpp

--- ПР1/Triangle/main.cpp
+++ ПР1/Triangle/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include "triangle.h"
 
+int main() {
+	Triangle tr[3];
+	int i = 0;
+	while (i < 3){
+		double a, b, c;
+		std::cin >> a >> b >> c;
+		Triangle temp(a, b, c);
+		if (!temp.exst_tr()){
+			std::cout << "Triangle doesn't exist." << '\n';
+		} else {tr[i] = temp; i++;}
+	}
+	for (int i = 0; i < 3; i++) std::cout << tr[i].perimeter() << ' ' << tr[i].square();
+}
