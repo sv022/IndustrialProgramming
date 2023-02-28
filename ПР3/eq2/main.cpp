9d5f757eaaa96594d8f89d7d02b62c64691a549f
sv022 <83920644+sv022@users.noreply.github.com>
2023-02-28 15:10:15 +0300

    Main

---
+++ ПР3/eq2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include "eq2.h"
+
+using namespace std;
+
+int main() {
+	eq2 P(1, 2, -3);
+	eq2 Q(1, 2, 1);
+	eq2 L = P + Q;
+	L.find_x();
+  L.find_y(0);
+}
