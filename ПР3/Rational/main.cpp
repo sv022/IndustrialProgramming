329375926bbb72162492b264c033208be1976975
sv022 <83920644+sv022@users.noreply.github.com>
2023-03-04 16:10:07 +0300

    Update main.cpp

--- ПР3/Rational/main.cpp
+++ ПР3/Rational/main.cpp
@@ -3,22 +3,36 @@
 #include <numeric>
 #include "rational.h"
 
+// operator -
+
+Rational operator - (Rational &ths, Rational &other){
+        return Rational((ths.n * other.m - other.n * ths.m), (ths.m * other.m));
+}
+Rational operator - (Rational &ths, int &other){
+    return Rational((ths.n - other * ths.m), (ths.m));
+}
+
+
 using namespace std;
 
-void demo(Rational a){
+void demo(Rational a, int i){
 	Rational b(2, 7);
-	Rational c1 = a + b;
-	Rational c2 = a - b;
+	Rational c1 = a + b * std::pow(-1, i);
+	Rational c2 = a + b * std::pow(-1, i + 1);
+	cout << "c1 ";
 	c1.show();
+	cout << "c2 ";
 	c2.show();
 	++c1;
+	cout << "c1++ ";
 	c1.show();
-	cout << (c1 == c2)<< '\n';
-	cout << (c1 > c2) << '\n';
+	cout << "c1 == c2 " << (c1 == c2) << '\n';
+	cout << "c1 > c2 " << (c1 > c2) << '\n';
 	c1 += c2;
+	cout << "c1 += c2 ";
 	c1.show();
-	c1 = Rational(9, 13);
-	c1.show();
+	c1.set(9, 13);
+	cout << "\n";
 }
 
 int main() {
@@ -32,6 +46,6 @@ int main() {
 		b = (i + 3 + (i % 2));
 		arr[i] = Rational(a, b);
 		cout << "Fraction " << i + 1 << '\n';
-		demo(arr[i]);
+		demo(arr[i], i);
 	}
 }
